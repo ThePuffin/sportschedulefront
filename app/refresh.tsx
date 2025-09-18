7ac762625b11170943b4b4281a901055fd26d14c
ThePuffin <ballutvincent@gmail.com>
2025-09-18 17:53:23 +0200

    add refresh team button

--- app/refresh.tsx
+++ app/refresh.tsx
@@ -52,6 +52,24 @@ export default function GameofTheDay() {
     }
   };
 
+  const refreshTeams = async (): Promise<void> => {
+    console.log(`Refreshing teams for all leagues`);
+    setIsLoading(true);
+
+    try {
+      const response = await fetch(`${EXPO_PUBLIC_API_BASE_URL}/teams/refresh`, {
+        method: 'POST',
+      });
+      console.log(`Response status for teams:`, response);
+      return;
+    } catch (error) {
+      console.error(`Error fetching games for teams:`, error);
+      return;
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   const [leaguesAvailable, setLeaguesAvailable] = useState<string[]>([]);
   const [isLoading, setIsLoading] = useState(true);
 
@@ -91,6 +109,31 @@ export default function GameofTheDay() {
           minHeight: '100vh',
         }}
       >
+        <button
+          onClick={() => refreshTeams()}
+          disabled={isLoading}
+          style={{
+            padding: '10px 20px',
+            borderRadius: '8px',
+            border: 'none',
+            backgroundColor: isLoading ? '#ccc' : '#007bff',
+            color: '#fff',
+            fontSize: '16px',
+            cursor: isLoading ? 'not-allowed' : 'pointer',
+            boxShadow: '0 2px 6px rgba(0,0,0,0.1)',
+            transition: 'background 0.2s',
+            width: width < 600 ? '100%' : '300px',
+            maxWidth: '300px',
+            minWidth: '300px',
+            display: 'block',
+          }}
+        >
+          <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }}>
+            <ThemedText style={{ color: '#fff', fontSize: '16px', marginRight: 10 }}>TEAMS</ThemedText>
+          </View>
+        </button>
+        <br />
+        <hr style={{ width: '300px' }} />
         {leaguesAvailable.map((league) => (
           <View
             key={league}
