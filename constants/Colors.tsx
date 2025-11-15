b83dc69c286e36264e223a002c53b05c3950f4ca
ThePuffin <ballutvincent@gmail.com>
2025-11-15 11:17:47 +0100

    reorganize color

--- constants/Colors.tsx
+++ constants/Colors.tsx
@@ -27,285 +27,125 @@ export const Colors = {
     color: '#ffffff',
     backgroundColor: '#000000',
   },
-  'NHL-NJ': {
-    color: '#000000',
-    backgroundColor: '#ce1126',
-  },
-  'NHL-NJD': {
-    color: '#000000',
-    backgroundColor: '#ce1126',
-  },
-  'NHL-NYI': {
-    color: '#00539b',
-    backgroundColor: '#f47d30',
-  },
-  'NHL-NYR': {
-    color: '#0033a0',
-    backgroundColor: '#c8102e',
-  },
-  'NHL-PHI': {
-    color: '#000000',
-    backgroundColor: '#f74902',
-  },
-  'NHL-PIT': {
-    color: '#000000',
-    backgroundColor: '#fcb514',
-  },
-  'NHL-BOS': {
-    color: '#000000',
-    backgroundColor: '#ffb81c',
-  },
-  'NHL-BUF': {
-    color: '#ffb81c',
-    backgroundColor: '#003087',
-  },
-  'NHL-MTL': {
-    color: '#af1e2d',
-    backgroundColor: '#192168',
-  },
-  'NHL-OTT': {
-    color: '#b79257',
-    backgroundColor: '#da1a32',
-  },
-  'NHL-TOR': {
-    color: '#ffffff',
-    backgroundColor: '#00205b',
-  },
-  'NHL-CAR': {
-    color: '#ffffff',
-    backgroundColor: '#ce1126',
-  },
-  'NHL-FLA': {
-    color: '#041e42',
-    backgroundColor: '#c8102e',
-  },
-  'NHL-TB': {
-    backgroundColor: '#002868',
-    color: '#ffffff',
-  },
-  'NHL-TBL': {
-    backgroundColor: '#002868',
-    color: '#ffffff',
-  },
-  'NHL-WSH': {
-    color: '#c8102e',
-    backgroundColor: '#041e42',
-  },
-  'NHL-CHI': {
-    color: '#000000',
-    backgroundColor: '#cf0a2c',
-  },
-  'NHL-DET': {
-    color: '#ffffff',
-    backgroundColor: '#ce1126',
-  },
-  'NHL-NSH': {
-    color: '#ffb81c',
-    backgroundColor: '#041e42',
-  },
-  'NHL-STL': {
-    color: '#fcb514',
-    backgroundColor: '#0000ff',
-  },
-  'NHL-CGY': {
-    color: '#faaf19',
-    backgroundColor: '#d2001c',
-  },
-  'NHL-COL': {
-    color: '#a2aaad',
-    backgroundColor: '#6f263d',
-  },
-  'NHL-EDM': {
-    color: '#ff4c00',
-    backgroundColor: '#041e42',
-  },
-  'NHL-VAN': {
-    color: '#00843d',
-    backgroundColor: '#00205b',
-  },
-  'NHL-ANA': {
-    color: '#000000',
-    backgroundColor: '#f47a38',
-  },
-  'NHL-DAL': {
-    backgroundColor: '#8f8f8c',
-    color: '#006847',
-  },
-  'NHL-LA': {
-    color: '#a2aaad',
-    backgroundColor: '#111111',
-  },
-  'NHL-LAK': {
-    color: '#a2aaad',
-    backgroundColor: '#111111',
-  },
-  'NHL-SJ': {
-    color: '#ea7200',
-    backgroundColor: '#006d75',
-  },
-  'NHL-SJS': {
-    color: '#ea7200',
-    backgroundColor: '#006d75',
-  },
-  'NHL-CBJ': {
-    color: '#ce1126',
-    backgroundColor: '#002654',
-  },
-  'NHL-MIN': {
-    color: '#a6192e',
-    backgroundColor: '#154734',
-  },
-  'NHL-WPG': {
-    color: '#55565a',
-    backgroundColor: '#041e42',
-  },
-  'NHL-UTA': {
-    color: '#090909',
-    backgroundColor: '#71afe5',
-  },
-  'NHL-ARI': {
-    color: '#e2d6b5',
-    backgroundColor: '#8c2633',
-  },
-  'NHL-VG': {
-    color: '#b4975a',
-    backgroundColor: '#333f42',
-  },
-  'NHL-VGK': {
-    color: '#b4975a',
-    backgroundColor: '#333f42',
-  },
-  'NHL-SEA': {
-    color: '#99d9d9',
-    backgroundColor: '#001628',
-  },
-  'NFL-ARI': {
-    backgroundColor: '#a4113e',
-    color: '#ffffff',
-  },
-  'NFL-ATL': {
-    color: '#a71930',
+  'MLB-ARI': {
+    color: '#aa1f2e',
     backgroundColor: '#000000',
   },
-  'NFL-BAL': {
-    color: '#29126f',
+  'MLB-ATH': {
+    color: '#006241',
+    backgroundColor: '#efb21e',
+  },
+  'MLB-ATL': {
+    color: '#0c2340',
+    backgroundColor: '#ba0c2f',
+  },
+  'MLB-BAL': {
+    color: '#df4601',
     backgroundColor: '#000000',
   },
-  'NFL-BUF': {
-    color: '#00338d',
-    backgroundColor: '#d50a0a',
+  'MLB-BOS': {
+    backgroundColor: '#0d2b56',
+    color: '#bd3039',
   },
-  'NFL-CAR': {
-    color: '#0085ca',
-    backgroundColor: '#000000',
+  'MLB-CHC': {
+    color: '#0e3386',
+    backgroundColor: '#cc3433',
   },
-  'NFL-CHI': {
-    color: '#0b1c3a',
-    backgroundColor: '#e64100',
-  },
-  'NFL-CIN': {
-    color: '#fb4f14',
-    backgroundColor: '#000000',
-  },
-  'NFL-CLE': {
-    color: '#472a08',
-    backgroundColor: '#ff3c00',
-  },
-  'NFL-DAL': {
-    color: '#002a5c',
-    backgroundColor: '#b0b7bc',
-  },
-  'NFL-DEN': {
-    color: '#0a2343',
-    backgroundColor: '#fc4c02',
-  },
-  'NFL-DET': {
-    color: '#0076b6',
-    backgroundColor: '#bbbbbb',
-  },
-  'NFL-GB': {
-    color: '#204e32',
-    backgroundColor: '#ffb612',
-  },
-  'NFL-HOU': {
-    color: '#00143f',
-    backgroundColor: '#c41230',
-  },
-  'NFL-IND': {
-    color: '#ffffff',
-    backgroundColor: '#003b75',
-  },
-  'NFL-JAX': {
-    color: '#007487',
-    backgroundColor: '#d7a22a',
-  },
-  'NFL-KC': {
-    color: '#e31837',
-    backgroundColor: '#ffb612',
-  },
-  'NFL-LV': {
+  'MLB-CHW': {
     color: '#000000',
-    backgroundColor: '#a5acaf',
+    backgroundColor: '#c4ced4',
   },
-  'NFL-LAC': {
-    color: '#0080c6',
-    backgroundColor: '#ffc20e',
-  },
-  'NFL-LAR': {
-    color: '#003594',
-    backgroundColor: '#ffd100',
-  },
-  'NFL-MIA': {
-    color: '#008e97',
-    backgroundColor: '#fc4c02',
-  },
-  'NFL-MIN': {
-    color: '#4f2683',
-    backgroundColor: '#ffc62f',
-  },
-  'NFL-NE': {
-    color: '#002a5c',
-    backgroundColor: '#c60c30',
-  },
-  'NFL-NO': {
-    color: '#d3bc8d',
-    backgroundColor: '#000000',
-  },
-  'NFL-NYG': {
-    color: '#003c7f',
-    backgroundColor: '#c9243f',
-  },
-  'NFL-NYJ': {
-    backgroundColor: '#115740',
+  'MLB-CIN': {
     color: '#ffffff',
+    backgroundColor: '#c6011f',
   },
-  'NFL-PHI': {
-    color: '#06424d',
+  'MLB-CLE': {
+    backgroundColor: '#002b5c',
+    color: '#e31937',
+  },
+  'MLB-COL': {
+    backgroundColor: '#33006f',
+    color: '#000000',
+  },
+  'MLB-DET': {
+    color: '#002d5c',
+    backgroundColor: '#ff6600',
+  },
+  'MLB-HOU': {
+    color: '#002d62',
+    backgroundColor: '#eb6e1f',
+  },
+  'MLB-KC': {
+    color: '#004687',
+    backgroundColor: '#7ab2dd',
+  },
+  'MLB-LAA': {
+    color: '#ba0021',
+    backgroundColor: '#c4ced4',
+  },
+  'MLB-LAD': {
+    color: '#ffffff',
+    backgroundColor: '#005a9c',
+  },
+  'MLB-MIA': {
+    color: '#00a3e0',
     backgroundColor: '#000000',
   },
-  'NFL-PIT': {
-    color: '#000000',
-    backgroundColor: '#ffb612',
+  'MLB-MIL': {
+    color: '#13294b',
+    backgroundColor: '#ffc72c',
   },
-  'NFL-SF': {
-    color: '#aa0000',
-    backgroundColor: '#b3995d',
+  'MLB-MIN': {
+    color: '#031f40',
+    backgroundColor: '#e20e32',
   },
-  'NFL-SEA': {
-    color: '#002a5c',
-    backgroundColor: '#69be28',
+  'MLB-NYM': {
+    backgroundColor: '#002d72',
+    color: '#ff5910',
   },
-  'NFL-TB': {
-    color: '#bd1c36',
-    backgroundColor: '#3e3a35',
+  'MLB-NYY': {
+    color: '#132448',
+    backgroundColor: '#c4ced4',
   },
-  'NFL-TEN': {
-    color: '#4b92db',
-    backgroundColor: '#002a5c',
+  'MLB-PHI': {
+    color: '#e81828',
+    backgroundColor: '#003278',
   },
-  'NFL-WSH': {
-    color: '#5a1414',
-    backgroundColor: '#ffb612',
+  'MLB-PIT': {
+    backgroundColor: '#000000',
+    color: '#fdb827',
+  },
+  'MLB-SD': {
+    color: '#3e2312',
+    backgroundColor: '#ffc425',
+  },
+  'MLB-SF': {
+    color: '#fd5a1e',
+    backgroundColor: '#000000',
+  },
+  'MLB-SEA': {
+    color: '#005c5c',
+    backgroundColor: '#0c2c56',
+  },
+  'MLB-STL': {
+    color: '#be0a14',
+    backgroundColor: '#001541',
+  },
+  'MLB-TB': {
+    color: '#092c5c',
+    backgroundColor: '#8fbce6',
+  },
+  'MLB-TEX': {
+    backgroundColor: '#003278',
+    color: '#c0111f',
+  },
+  'MLB-TOR': {
+    color: '#134a8e',
+    backgroundColor: '#6cace5',
+  },
+  'MLB-WSH': {
+    color: '#ab0003',
+    backgroundColor: '#11225b',
   },
   'NBA-ATL': {
     color: '#c8102e',
@@ -427,125 +267,285 @@ export const Colors = {
     color: '#e31837',
     backgroundColor: '#002b5c',
   },
-  'MLB-ARI': {
-    color: '#aa1f2e',
-    backgroundColor: '#000000',
-  },
-  'MLB-ATL': {
-    color: '#0c2340',
-    backgroundColor: '#ba0c2f',
-  },
-  'MLB-BAL': {
-    color: '#df4601',
-    backgroundColor: '#000000',
-  },
-  'MLB-BOS': {
-    backgroundColor: '#0d2b56',
-    color: '#bd3039',
-  },
-  'MLB-CHC': {
-    color: '#0e3386',
-    backgroundColor: '#cc3433',
-  },
-  'MLB-CHW': {
-    color: '#000000',
-    backgroundColor: '#c4ced4',
-  },
-  'MLB-CIN': {
+  'NFL-ARI': {
+    backgroundColor: '#a4113e',
     color: '#ffffff',
-    backgroundColor: '#c6011f',
   },
-  'MLB-CLE': {
-    backgroundColor: '#002b5c',
-    color: '#e31937',
+  'NFL-ATL': {
+    color: '#a71930',
+    backgroundColor: '#000000',
   },
-  'MLB-COL': {
-    backgroundColor: '#33006f',
-    color: '#000000',
+  'NFL-BAL': {
+    backgroundColor: '#29126f',
+    color: '#9E7C0C',
   },
-  'MLB-DET': {
-    color: '#002d5c',
-    backgroundColor: '#ff6600',
+  'NFL-BUF': {
+    color: '#00338d',
+    backgroundColor: '#d50a0a',
   },
-  'MLB-HOU': {
-    color: '#002d62',
-    backgroundColor: '#eb6e1f',
+  'NFL-CAR': {
+    color: '#0085ca',
+    backgroundColor: '#000000',
   },
-  'MLB-KC': {
-    color: '#004687',
-    backgroundColor: '#7ab2dd',
+  'NFL-CHI': {
+    color: '#0b1c3a',
+    backgroundColor: '#e64100',
   },
-  'MLB-LAA': {
-    color: '#ba0021',
-    backgroundColor: '#c4ced4',
+  'NFL-CIN': {
+    color: '#fb4f14',
+    backgroundColor: '#000000',
   },
-  'MLB-LAD': {
+  'NFL-CLE': {
+    color: '#472a08',
+    backgroundColor: '#ff3c00',
+  },
+  'NFL-DAL': {
+    color: '#002a5c',
+    backgroundColor: '#b0b7bc',
+  },
+  'NFL-DEN': {
+    color: '#0a2343',
+    backgroundColor: '#fc4c02',
+  },
+  'NFL-DET': {
+    color: '#0076b6',
+    backgroundColor: '#bbbbbb',
+  },
+  'NFL-GB': {
+    color: '#204e32',
+    backgroundColor: '#ffb612',
+  },
+  'NFL-HOU': {
+    color: '#00143f',
+    backgroundColor: '#c41230',
+  },
+  'NFL-IND': {
     color: '#ffffff',
-    backgroundColor: '#005a9c',
+    backgroundColor: '#003b75',
   },
-  'MLB-MIA': {
-    color: '#00a3e0',
+  'NFL-JAX': {
+    color: '#007487',
+    backgroundColor: '#d7a22a',
+  },
+  'NFL-KC': {
+    color: '#e31837',
+    backgroundColor: '#ffb612',
+  },
+  'NFL-LAC': {
+    color: '#0080c6',
+    backgroundColor: '#ffc20e',
+  },
+  'NFL-LAR': {
+    color: '#003594',
+    backgroundColor: '#ffd100',
+  },
+  'NFL-LV': {
+    color: '#000000',
+    backgroundColor: '#a5acaf',
+  },
+  'NFL-MIA': {
+    color: '#008e97',
+    backgroundColor: '#fc4c02',
+  },
+  'NFL-MIN': {
+    color: '#4f2683',
+    backgroundColor: '#ffc62f',
+  },
+  'NFL-NE': {
+    color: '#002a5c',
+    backgroundColor: '#c60c30',
+  },
+  'NFL-NO': {
+    color: '#d3bc8d',
     backgroundColor: '#000000',
   },
-  'MLB-MIL': {
-    color: '#13294b',
-    backgroundColor: '#ffc72c',
+  'NFL-NYG': {
+    color: '#003c7f',
+    backgroundColor: '#c9243f',
   },
-  'MLB-MIN': {
-    color: '#031f40',
-    backgroundColor: '#e20e32',
+  'NFL-NYJ': {
+    backgroundColor: '#115740',
+    color: '#ffffff',
   },
-  'MLB-NYM': {
-    backgroundColor: '#002d72',
-    color: '#ff5910',
-  },
-  'MLB-NYY': {
-    color: '#132448',
-    backgroundColor: '#c4ced4',
-  },
-  'MLB-ATH': {
-    color: '#006241',
-    backgroundColor: '#efb21e',
-  },
-  'MLB-PHI': {
-    color: '#e81828',
-    backgroundColor: '#003278',
-  },
-  'MLB-PIT': {
-    backgroundColor: '#000000',
-    color: '#fdb827',
-  },
-  'MLB-SD': {
-    color: '#3e2312',
-    backgroundColor: '#ffc425',
-  },
-  'MLB-SF': {
-    color: '#fd5a1e',
+  'NFL-PHI': {
+    color: '#A5ACAF',
     backgroundColor: '#000000',
   },
-  'MLB-SEA': {
-    color: '#005c5c',
-    backgroundColor: '#0c2c56',
+  'NFL-PIT': {
+    color: '#000000',
+    backgroundColor: '#ffb612',
   },
-  'MLB-STL': {
-    color: '#be0a14',
-    backgroundColor: '#001541',
+  'NFL-SF': {
+    color: '#aa0000',
+    backgroundColor: '#b3995d',
   },
-  'MLB-TB': {
-    color: '#092c5c',
-    backgroundColor: '#8fbce6',
+  'NFL-SEA': {
+    color: '#002a5c',
+    backgroundColor: '#69be28',
   },
-  'MLB-TEX': {
-    backgroundColor: '#003278',
-    color: '#c0111f',
+  'NFL-TB': {
+    color: '#bd1c36',
+    backgroundColor: '#3e3a35',
   },
-  'MLB-TOR': {
-    color: '#134a8e',
-    backgroundColor: '#6cace5',
+  'NFL-TEN': {
+    color: '#4b92db',
+    backgroundColor: '#002a5c',
   },
-  'MLB-WSH': {
-    color: '#ab0003',
-    backgroundColor: '#11225b',
+  'NFL-WSH': {
+    color: '#5a1414',
+    backgroundColor: '#ffb612',
+  },
+  'NHL-ANA': {
+    color: '#000000',
+    backgroundColor: '#f47a38',
+  },
+  'NHL-ARI': {
+    color: '#e2d6b5',
+    backgroundColor: '#8c2633',
+  },
+  'NHL-BOS': {
+    color: '#000000',
+    backgroundColor: '#ffb81c',
+  },
+  'NHL-BUF': {
+    color: '#ffb81c',
+    backgroundColor: '#003087',
+  },
+  'NHL-CAR': {
+    color: '#ffffff',
+    backgroundColor: '#ce1126',
+  },
+  'NHL-CBJ': {
+    color: '#ce1126',
+    backgroundColor: '#002654',
+  },
+  'NHL-CGY': {
+    color: '#faaf19',
+    backgroundColor: '#d2001c',
+  },
+  'NHL-CHI': {
+    color: '#000000',
+    backgroundColor: '#cf0a2c',
+  },
+  'NHL-COL': {
+    color: '#a2aaad',
+    backgroundColor: '#6f263d',
+  },
+  'NHL-DAL': {
+    backgroundColor: '#8f8f8c',
+    color: '#006847',
+  },
+  'NHL-DET': {
+    color: '#ffffff',
+    backgroundColor: '#ce1126',
+  },
+  'NHL-EDM': {
+    color: '#ff4c00',
+    backgroundColor: '#041e42',
+  },
+  'NHL-FLA': {
+    color: '#041e42',
+    backgroundColor: '#c8102e',
+  },
+  'NHL-LA': {
+    color: '#a2aaad',
+    backgroundColor: '#111111',
+  },
+  'NHL-LAK': {
+    color: '#a2aaad',
+    backgroundColor: '#111111',
+  },
+  'NHL-MIN': {
+    color: '#a6192e',
+    backgroundColor: '#154734',
+  },
+  'NHL-MTL': {
+    color: '#af1e2d',
+    backgroundColor: '#192168',
+  },
+  'NHL-NJ': {
+    color: '#000000',
+    backgroundColor: '#ce1126',
+  },
+  'NHL-NJD': {
+    color: '#000000',
+    backgroundColor: '#ce1126',
+  },
+  'NHL-NSH': {
+    color: '#ffb81c',
+    backgroundColor: '#041e42',
+  },
+  'NHL-NYI': {
+    color: '#00539b',
+    backgroundColor: '#f47d30',
+  },
+  'NHL-NYR': {
+    color: '#0033a0',
+    backgroundColor: '#c8102e',
+  },
+  'NHL-OTT': {
+    color: '#b79257',
+    backgroundColor: '#da1a32',
+  },
+  'NHL-PHI': {
+    color: '#000000',
+    backgroundColor: '#f74902',
+  },
+  'NHL-PIT': {
+    color: '#000000',
+    backgroundColor: '#fcb514',
+  },
+  'NHL-SEA': {
+    color: '#99d9d9',
+    backgroundColor: '#001628',
+  },
+  'NHL-SJ': {
+    color: '#ea7200',
+    backgroundColor: '#006d75',
+  },
+  'NHL-SJS': {
+    color: '#ea7200',
+    backgroundColor: '#006d75',
+  },
+  'NHL-STL': {
+    color: '#fcb514',
+    backgroundColor: '#0000ff',
+  },
+  'NHL-TB': {
+    backgroundColor: '#002868',
+    color: '#ffffff',
+  },
+  'NHL-TBL': {
+    backgroundColor: '#002868',
+    color: '#ffffff',
+  },
+  'NHL-TOR': {
+    color: '#ffffff',
+    backgroundColor: '#00205b',
+  },
+  'NHL-UTA': {
+    color: '#090909',
+    backgroundColor: '#71afe5',
+  },
+  'NHL-VAN': {
+    color: '#00843d',
+    backgroundColor: '#00205b',
+  },
+  'NHL-VG': {
+    color: '#b4975a',
+    backgroundColor: '#333f42',
+  },
+  'NHL-VGK': {
+    color: '#b4975a',
+    backgroundColor: '#333f42',
+  },
+  'NHL-WPG': {
+    color: '#55565a',
+    backgroundColor: '#041e42',
+  },
+  'NHL-WSH': {
+    color: '#c8102e',
+    backgroundColor: '#041e42',
   },
   'WNBA-ATL': {
     color: '#e31837',
@@ -575,13 +575,13 @@ export const Colors = {
     color: '#002d62',
     backgroundColor: '#e03a3e',
   },
-  'WNBA-LV': {
-    color: '#a7a8aa',
-    backgroundColor: '#000000',
-  },
   'WNBA-LA': {
     color: '#552583',
     backgroundColor: '#fdb927',
+  },
+  'WNBA-LV': {
+    color: '#a7a8aa',
+    backgroundColor: '#000000',
   },
   'WNBA-MIN': {
     color: '#266092',
